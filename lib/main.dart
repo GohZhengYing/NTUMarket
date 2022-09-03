0c48abd523471b7b0b8d6a9e3de70898c6dc47f2
CasperCheong <ywcheong45@gmail.com>
2022-09-03 16:59:02 +0800

    LandingPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'loginScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,9 +11,56 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
     return MaterialApp(
-      home: Text('Hello!'),
+      title: 'Landing Page',
+      home: LoginScreen(),
     );
-    throw UnimplementedError();
   }
 }
+
+// class MyHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return Scaffold(
+//         body: Center(
+//       child: Container(
+//         height: 800, width: 300, color: Colors.blue,
+//         child: Stack(children: <Widget>[
+//           Container(
+//             width: 200,
+//             height: 500,
+//             color: Colors.red,
+//           ),
+//           Container(
+//             width: 200,
+//             height: 300,
+//             color: Colors.black,
+//           )
+//         ]),
+//         // child: Text(
+//         //   'NTUMarket',
+//         //   style: TextStyle(
+//         //     color: Color.fromARGB(255, 7, 0, 95),
+//         //     fontSize: 50,
+//         //     fontWeight: FontWeight.bold,
+//         //   ),
+//       ),
+//     ));
+//   }
+// }
+
+// class buildBackGround extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return Center(
+//       child: Container(
+//         width: 400,
+//         height: 800,
+//         color: Colors.grey,
+//       ),
+//     );
+//   }
+// }
