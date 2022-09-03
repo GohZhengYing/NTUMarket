9beb2da43bb5fa174ec44b9f1bedda9691047877
CasperCheong <ywcheong45@gmail.com>
2022-09-03 17:41:12 +0800

    Login

--- lib/loginScreen.dart
+++ lib/loginScreen.dart
@@ -15,7 +15,7 @@ Widget buildEmail() {
       Text(
         'Email',
         style: TextStyle(
-          color: Colors.white,
+          color: Colors.grey,
           fontSize: 16,
           fontWeight: FontWeight.bold,
         ),
@@ -25,7 +25,7 @@ Widget buildEmail() {
           alignment: Alignment.centerLeft,
           decoration: BoxDecoration(
               color: Colors.white,
-              borderRadius: BorderRadius.circular(8),
+              borderRadius: BorderRadius.circular(30),
               boxShadow: [
                 BoxShadow(
                     color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
@@ -39,12 +39,112 @@ Widget buildEmail() {
                 contentPadding: EdgeInsets.only(top: 14),
                 prefixIcon: Icon(
                   Icons.email,
-                  color: Color(0xff5ac18e),
+                  color: Color.fromARGB(255, 71, 231, 213),
                 ),
                 hintText: 'Email',
                 hintStyle: TextStyle(color: Colors.black38)),
           ))
     ],
+  );
+}
+
+Widget buildPassword() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        'Password',
+        style: TextStyle(
+          color: Colors.grey,
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      SizedBox(height: 10),
+      Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
+              ]),
+          height: 60,
+          child: TextField(
+            obscureText: true,
+            style: TextStyle(color: Colors.black87),
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Color.fromARGB(255, 71, 231, 213),
+                ),
+                hintText: 'Password',
+                hintStyle: TextStyle(color: Colors.black38)),
+          ))
+    ],
+  );
+}
+
+Widget buildForgetPasswordButton() {
+  return Container(
+    alignment: Alignment.center,
+    child: TextButton(
+      style: TextButton.styleFrom(
+        textStyle: const TextStyle(
+          fontSize: 20,
+          color: Colors.black12,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onPressed: () => print("Forgot Password Pressed"),
+      child: const Text(
+        'Forget Password',
+        style: TextStyle(
+            color: Color.fromARGB(255, 1, 78, 68)), //forget Password text color
+      ),
+    ),
+  );
+}
+
+Widget buildLoginButton() {
+  return Container(
+    padding: EdgeInsets.symmetric(vertical: 25),
+    width: double.infinity,
+    child: ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor:
+            MaterialStatePropertyAll<Color>(Color.fromARGB(255, 71, 231, 213)),
+      ),
+      onPressed: () => print("Login Pressed"),
+      child: Text(
+        "Login",
+        style: TextStyle(
+            color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold),
+      ),
+    ),
+  );
+}
+
+Widget buildSignUpButton() {
+  return GestureDetector(
+    onTap: () => print("Sign Up Pressed"),
+    child: RichText(
+      text: TextSpan(children: [
+        TextSpan(
+            text: "Don\'t have an account?",
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.w300)),
+        TextSpan(
+            text: 'Sign Up',
+            style: TextStyle(
+                color: Colors.green, fontSize: 18, fontWeight: FontWeight.bold))
+      ]),
+    ),
   );
 }
 
@@ -71,6 +171,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       Color.fromRGBO(147, 236, 227, 0.44),
                     ])),
                 child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
                   padding: EdgeInsets.symmetric(
                     horizontal: 25,
                     vertical: 120,
@@ -86,9 +187,16 @@ class _LoginScreenState extends State<LoginScreen> {
                             fontWeight: FontWeight.bold),
                       ),
                       SizedBox(
-                        height: 50,
+                        height: 120,
                       ),
                       buildEmail(),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      buildPassword(),
+                      buildForgetPasswordButton(),
+                      buildLoginButton(),
+                      buildSignUpButton(),
                     ],
                   ),
                 ),
